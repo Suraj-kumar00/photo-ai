cd666e3eb723b40cdfd952c3f93ed62943753171
Suraj <suraj.ukumar.p@gmail.com>
2025-03-15 22:44:01 +0530

    feat: fix backend dependenices

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,33 +1,71 @@
 import express from "express";
-import { TrainModel, GenerateImage, GenerateImagesFromPack } from "common/types";
-import prisma from "db";
+import {
+  TrainModel,
+  GenerateImage,
+  GenerateImagesFromPack,
+} from "../../packages/common/types.js";
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
+
+// Initialize express app
 const app = express();
+app.use(express.json());
 
-const PORT = process.env.PORT || 8080;
+// Set port
+const PORT = process.env.PORT || 8000;
 
+// Check database connection
+function checkDatabaseConnection() {
+  try {
+    prisma.$connect();
+    console.log("✅ PostgreSQL connected via supabase");
+  } catch (error) {
+    console.error("❌ Database connection error:", error);
+    process.exit(1);
+  }
+}
 
-app.post("/ai/training", (req, res) => {
+// Routes for AI training, generation, and pack generation
+app.post("/ai/training", async (req, res) => {
+  const parsedBody = TrainModel.safeParse(req.body);
 
-})
+  if (!parsedBody.success) {
+    res.status(400).json({ errorMessage: parsedBody.error.message });
+    return;
+  }
+
+  const data = await prisma.model.create({
+    data: {
+      name: parsedBody.data.name,
+      type: parsedBody.data.type,
+      age: parsedBody.data.age,
+      ethinicity: parsedBody.data.ethinicity,
+      eyeColor: parsedBody.data.eyeColor,
+      bald: parsedBody.data.bald,
+      zipUrl: parsedBody.data.zipUrl,
+      userId: "asdf",
+      falAiRequestId: "asdf",
+    },
+  });
+  res.json({
+    message: "Model trained successfully",
+    modelId: data.id,
+  });
+});
 
 app.post("/ai/generate", (req, res) => {
-
-})
-
-app.post("/pack/generate", (req, res) => {
-
-})
-
-app.get("/pack/bulk", (req, res) => {
   
 });
 
+app.post("/pack/generate", (req, res) => {});
 
-app.get("/image", (req, res) => {
+app.get("/pack/bulk", (req, res) => {});
 
-})
+app.get("/image", (req, res) => {});
 
+// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  checkDatabaseConnection();
 });
